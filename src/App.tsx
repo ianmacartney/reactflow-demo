e7a2a77671715387212c60134c5a569c0a2ed3ba
Ian Macartney <ianhmacartney@gmail.com>
2025-03-11 10:39:21 -0700

    move auth to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useAuthActions } from "@convex-dev/auth/react";
 import {
   Authenticated,
   Unauthenticated,
-  useConvexAuth,
   useMutation,
   useQuery,
 } from "convex/react";
@@ -26,6 +24,7 @@ import ReactFlow, {
   ReactFlowInstance,
 } from "reactflow";
 import { api } from "../convex/_generated/api";
+import { SignInForm, SignOutButton } from "./Auth";
 
 export default function App() {
   return (
@@ -49,92 +48,6 @@ export default function App() {
         </Unauthenticated>
       </main>
     </>
-  );
-}
-
-function SignOutButton() {
-  const { isAuthenticated } = useConvexAuth();
-  const { signOut } = useAuthActions();
-  return (
-    <>
-      {isAuthenticated && (
-        <button
-          className="bg-slate-200 dark:bg-slate-800 text-foreground rounded-md px-2 py-1"
-          onClick={() => void signOut()}
-        >
-          Sign out
-        </button>
-      )}
-    </>
-  );
-}
-
-function SignInForm() {
-  const { signIn } = useAuthActions();
-  const [flow, setFlow] = useState<"signIn" | "signUp">("signIn");
-  const [error, setError] = useState<string | null>(null);
-  return (
-    <div className="flex flex-col gap-8 w-96 mx-auto">
-      <p>Log in to see the numbers</p>
-      <form
-        className="flex flex-col gap-2"
-        onSubmit={(e) => {
-          e.preventDefault();
-          const formData = new FormData(e.target as HTMLFormElement);
-          formData.set("flow", flow);
-          void signIn("password", formData).catch((error) => {
-            setError(error.message);
-          });
-        }}
-      >
-        <input
-          className="bg-background text-foreground rounded-md p-2 border-2 border-slate-200 dark:border-slate-800"
-          type="email"
-          name="email"
-          placeholder="Email"
-        />
-        <input
-          className="bg-background text-foreground rounded-md p-2 border-2 border-slate-200 dark:border-slate-800"
-          type="password"
-          name="password"
-          autoComplete="current-password"
-          placeholder="Password"
-        />
-        <button
-          className="bg-foreground text-background rounded-md"
-          type="submit"
-        >
-          {flow === "signIn" ? "Sign in" : "Sign up"}
-        </button>
-        <div className="flex flex-row gap-2">
-          <span>
-            {flow === "signIn"
-              ? "Don't have an account?"
-              : "Already have an account?"}
-          </span>
-          <span
-            className="text-foreground underline hover:no-underline cursor-pointer"
-            onClick={() => setFlow(flow === "signIn" ? "signUp" : "signIn")}
-          >
-            {flow === "signIn" ? "Sign up instead" : "Sign in instead"}
-          </span>
-        </div>
-        {error && (
-          <div className="bg-red-500/20 border-2 border-red-500/50 rounded-md p-2">
-            <p className="text-foreground font-mono text-xs">
-              Error signing in: {error}
-            </p>
-          </div>
-        )}
-      </form>
-      <p>Or, if you're just checking it out...</p>
-      <button
-        className="bg-foreground text-background rounded-md"
-        onClick={() => void signIn("anonymous")}
-      >
-        Log in anonymously
-      </button>
-    </div>
   );
 }
 
